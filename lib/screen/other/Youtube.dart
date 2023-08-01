4ac68f1512ea5ea89d926294d6602103fa90b2f5
plan11plan <tjtfy7028@gmail.com>
2023-08-01 22:15:37 +0900

    download v1

--- lib/screen/other/Youtube.dart
+++ lib/screen/other/Youtube.dart
@@ -9,14 +9,37 @@ class Youtube extends StatefulWidget {
 }
 
 class _YoutubeState extends State<Youtube> {
+  final TextEditingController _urlController = TextEditingController(text: 'https://www.youtube.com/');
+  late WebViewController _webViewController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _urlController,
+          onSubmitted: (url) {
+            if (!url.startsWith('http://') && !url.startsWith('https://')) {
+              url = 'http://' + url;
+            }
+            _webViewController.loadUrl(url);
+          },
+          decoration: InputDecoration(
+            hintText: 'Enter URL here...',
+          ),
+          keyboardType: TextInputType.url,
+          textInputAction: TextInputAction.go,
+          autocorrect: false, // 자동 수정 기능 비활성화
+        ),
+
+      ),
       body: WebView(
         initialUrl: 'https://www.youtube.com/',
         javascriptMode: JavascriptMode.unrestricted,
-    )
+        onWebViewCreated: (controller) {
+          _webViewController = controller;
+        },
+      ),
     );
   }
 }
