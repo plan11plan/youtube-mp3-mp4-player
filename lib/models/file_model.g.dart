2803d67a54f7129c77772cb30a788240cfd97a43
plan11plan <tjtfy7028@gmail.com>
2023-08-04 02:10:18 +0900

    download v2- complete 2

---
+++ lib/models/file_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'file_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class MediaFileAdapter extends TypeAdapter<MediaFile> {
+  @override
+  final int typeId = 0;
+
+  @override
+  MediaFile read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return MediaFile(
+      fields[0] as String,
+      fields[1] as String,
+      fields[2] as String,
+      fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, MediaFile obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.filePath)
+      ..writeByte(2)
+      ..write(obj.thumbnailPath)
+      ..writeByte(3)
+      ..write(obj.fileType);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MediaFileAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
